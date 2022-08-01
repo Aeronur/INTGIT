ec8d857d7de10da159fb3a9ad9831f8a52ebc122
Nur <aeronur93@gmail.com>
2022-08-01 23:14:13 +0400

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Nurullo Ergashev on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
